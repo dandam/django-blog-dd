1470dd6f9d8685e079980ce3c8dbac236bf7b278
Daniel Damkoehler <170141+dandam@users.noreply.github.com>
2023-12-13 13:33:26 -0800

    summernote for pretty editing

--- dandamblog/blog/admin.py
+++ dandamblog/blog/admin.py
@@ -3,18 +3,20 @@ from .models import Post
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
-
+    summernote_fields = {'content'}
 
 admin.site.register(Post, PostAdmin)
 
-class FlatPageAdmin(FlatPageAdmin):
+class FlatPageAdmin(SummernoteModelAdmin):
+    summernote_fields = {'content'}
     fieldsets = [
         (None, {'fields': ['title', 'url', 'content',]}),
         (
@@ -28,3 +30,4 @@ class FlatPageAdmin(FlatPageAdmin):
 
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
+
--- dandamblog/dandamblog/settings.py
+++ dandamblog/dandamblog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'blog',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
